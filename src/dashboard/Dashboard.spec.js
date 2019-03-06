aa00d888d068942e99af27b161d14ef636532340
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-06 15:02:19 -0700

    Write tests to check text content of buttons and make sure they are changing with state

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -12,7 +12,7 @@ describe('<Dashboard />', () => {
   });
 
   describe('toggleLocked()', () => {
-    it('Toggles locked state on click', () => {
+    it('Toggles locked state and updates display on click', () => {
       const {getByText} = render(<Dashboard closed={true} locked={false}/>);
       const lockBtn = getByText(/lock gate/i);
 
@@ -22,18 +22,43 @@ describe('<Dashboard />', () => {
       fireEvent.click(lockBtn);
       getByText(/unlocked/i);
     });
+
+    it('Toggles locked state and updates button text on click', () => {
+      const {getByText} = render(<Dashboard />);
+      const lockBtn = getByText(/lock gate/i);
+      const closeBtn = getByText(/close gate/i);
+
+      fireEvent.click(closeBtn);
+      getByText(/lock gate/i);
+      fireEvent.click(lockBtn);
+      getByText(/unlock gate/i);
+      fireEvent.click(lockBtn);
+      getByText(/lock gate/i);
+    });
   });
-});
 
-describe('toggleLocked()', () => {
-  it('Toggles locked state on click', () => {
-    const {getByText} = render(<Dashboard/>);
-    const closeBtn = getByText(/close gate/i);
+  describe('toggleLocked()', () => {
+    it('Toggles closed state and updates display on click', () => {
+      const {getByText} = render(<Dashboard/>);
+      const closeBtn = getByText(/close gate/i);
 
-    getByText(/open/i);
-    fireEvent.click(closeBtn);
-    getByText(/closed/i);
-    fireEvent.click(closeBtn);
-    getByText(/pen/i);
+
+      getByText(/open/i);
+      fireEvent.click(closeBtn);
+      getByText(/closed/i);
+      fireEvent.click(closeBtn);
+      getByText(/pen/i);
+    });
+
+    it('Toggles closed state and updates button text on click', () => {
+      const {getByText} = render(<Dashboard/>);
+      const closeBtn = getByText(/close gate/i);
+
+      getByText(/close gate/i);
+      fireEvent.click(closeBtn);
+      getByText(/open gate/i);
+      fireEvent.click(closeBtn);
+      getByText(/close gate/i);
+    });
   });
 });
