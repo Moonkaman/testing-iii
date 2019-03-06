5d0f4b82df0ed1115a35ce6bf3c810e13cd23020
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-06 14:27:52 -0700

    Write tests to make sure buttons and display work correctly

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import {render, fireEvent, cleanup} from 'react-testing-library';
 import renderer from 'react-test-renderer';
+import 'jest-dom/extend-expect';
 
 import Dashboard from './Dashboard';
 
@@ -7,5 +9,31 @@ describe('<Dashboard />', () => {
   it('checks snapshot to make sure nothing changed', () => {
     const tree = renderer.create(<Dashboard />).toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  describe('toggleLocked()', () => {
+    it('Toggles locked state on click', () => {
+      const {getByText} = render(<Dashboard closed={true} locked={false}/>);
+      const lockBtn = getByText(/lock gate/i);
+
+      getByText(/unlocked/i);
+      fireEvent.click(lockBtn);
+      getByText(/locked/i);
+      fireEvent.click(lockBtn);
+      getByText(/unlocked/i);
+    });
+  });
+});
+
+describe('toggleLocked()', () => {
+  it('Toggles locked state on click', () => {
+    const {getByText} = render(<Dashboard/>);
+    const closeBtn = getByText(/close gate/i);
+
+    getByText(/open/i);
+    fireEvent.click(closeBtn);
+    getByText(/closed/i);
+    fireEvent.click(closeBtn);
+    getByText(/pen/i);
   });
 });
