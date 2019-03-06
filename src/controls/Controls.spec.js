836c832335dd25181e46cf8cd625799e2a947260
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-06 13:51:19 -0700

    Create snapshot test for controls component

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,11 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Controls from './Controls';
+
+describe('<Controls />', () => {
+  it('Checks snapshot for controls', () => {
+    const tree = renderer.create(<Controls />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
