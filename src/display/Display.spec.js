f2661303d6976eee09aa404c02a886db9c6131bd
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-06 15:20:12 -0700

    Restructure display tests & create class checking tests for closed display

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-testing-library'
 import renderer from 'react-test-renderer';
+import 'jest-dom/extend-expect';
 
 import Display from './Display';
 
@@ -10,24 +11,38 @@ describe('<Display />', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  it('Displays correct default gate lock value', () => {
-    const {getByText} = render(<Display />);
-    getByText(/unlocked/i);
+  describe('Closed Display', () => {
+    it('Displays correct deflaut gate closed value', () => {
+      const {getByText} = render(<Display />);
+      getByText(/open/i);
+    });
+
+    it('Displays open text if open is true', () => {
+      const {getByText} = render(<Display closed={true} />)
+      getByText(/closed/i);
+    });
+
+    it('Has green-led class when open', () => {
+      const{getByText} = render(<Display />);
+      expect(getByText(/open/i)).toHaveClass('green-led')
+    });
+
+    it('Has red-led class when closed', () => {
+      const{getByText} = render(<Display closed={true} />);
+      expect(getByText(/closed/i)).toHaveClass('red-led')
+    });
   });
 
-  it('Displays correct deflaut gate position value', () => {
-    const {getByText} = render(<Display />);
-    getByText(/open/i);
-  });
+  describe('Locked Display', () => {
+    it('Displays correct default gate lock value', () => {
+      const {getByText} = render(<Display />);
+      getByText(/unlocked/i);
+    });
 
-  it('Displays open text if open is true', () => {
-    const {getByText} = render(<Display closed={true} />)
-    getByText(/closed/i);
-  });
-
-  it('Displays locked text if locked is true', () => {
-    const {getByText} = render(<Display closed={true} locked={true} />)
-    getByText(/locked/i);
+    it('Displays locked text if locked is true', () => {
+      const {getByText} = render(<Display closed={true} locked={true} />)
+      getByText(/locked/i);
+    });
   });
 });
 
