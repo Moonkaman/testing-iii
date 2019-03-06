80a264614e7e148a780afaf8dd8e8c2f96e33497
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-06 13:48:47 -0700

    Write snapshot test for display

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Display from './Display';
+
+describe('<Display />', () => {
+  it('Checks display against snapshot', () => {
+    const tree = renderer.create(<Display />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
